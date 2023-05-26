63003685a46d05ed939cdcda25036570039f19ee
Rodolfo Souza <rodolfomssouza@gmail.com>
2023-05-26 16:56:47 -0500

    rainfall generator

--- src/SoilMoisture.jl
+++ src/SoilMoisture.jl
@@ -1,5 +1,50 @@
 module SoilMoisture
 
 # Write your package code here.
+# %% Packages ----------------------------------------------------------------
+using Distributions
+
+
+# %% Exports -----------------------------------------------------------------
+export rainfall_poisson
+
+
+# %% Functions ---------------------------------------------------------------
+
+"""
+`rainfall_poisson(n, α, λ)`
+
+Generates rainfall using Poisson process.
+
+# Arguments
+- `n::Integer`: number of events.
+- `α::Float64`: mean rain depth.
+- `λ::Float64`: rain event interval (1/d).
+
+If rainfall needs to be similated in timescale smaller than daily, multiply `λ` by dt.
+
+# Example
+```
+nevents = 30
+α = 0.75
+λ = 0.45
+rain = rainfall_poisson(n, α, λ)
+```
+"""
+function rainfall_poisson(nevents::Int64, α::Float64, λ::Float64)
+    rain::Vector{Float64} = zeros(nevents)
+    r1::Float64 = 0.0
+    r2::Float64 = 0.0
+    for i = 1:nevents
+        r1 = rand(Uniform(0, 1))
+        if r1 < λ
+            r2 = rand(Uniform(0, 1))
+            rain[i] = -α * log((1 - r2))
+        else
+            rain[i] = 0
+        end
+    end
+    return rain
+end
 
 end
